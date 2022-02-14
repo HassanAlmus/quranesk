288dd988fd69093e8bd2c5e76dfa5b4345ce6494
Hassan Al Mosapeeh <hassanalmosapeeh@gmail.com>
2022-02-14 04:26:13 +0300

    k

--- pages/tafseerenamoona.tsx
+++ pages/tafseerenamoona.tsx
@@ -1,15 +1,17 @@
 import {GetServerSideProps} from "next";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "../styles/tafseerenamoona.module.scss";
 import Head from "next/head";
 import {NamoonaTopic} from "../utils";
-import { gql } from "@apollo/client"
-import client from "../apollo-client";
+import { gql, useClient} from "urql"
+import { useSnapshot, proxy } from "valtio"
+import {useRouter} from "next/router";
+import Loader from '../src/components/Loader';
 
-const returnQuery = (s : number, link : string) => {
-    return gql `
+const returnQuery = (link : string) => {
+    return gql`
         query Query {
-            namoonaTopic(s: ${s}, link: "${link}"){
+            namoonaTopic(link: "${link}"){
                 range
                 title
                 text
@@ -18,34 +20,52 @@ const returnQuery = (s : number, link : string) => {
     `;
 };
 
-const TafseereNamoona = (props : {
-    link: string,
-    s: number,
-    v: number,
-    data: {
-        namoonaTopic: NamoonaTopic
-    }
-}) => {
-    return (<>
+const state = proxy({
+    init: false,
+    data: null,
+    s: null,
+    v: null
+})
+
+const TafseereNamoona = () => {
+    const snap = useSnapshot(state);
+    const client = useClient();
+    const router = useRouter();
+
+    useEffect(()=>{
+      if(router.query.s&&router.query.v&&router.query.id){
+          console.log(Number(router.query.s)-1, router.query.id as string)
+          client.query(returnQuery(router.query.id as string)).toPromise().then(result=>{
+            if(result.error) console.log(result.error);
+            console.log(result.data)
+            state.data=result.data
+            state.s=Number(router.query.s)-1
+            state.v=Number(router.query.v)-1
+            state.init=true
+          })
+      }
+    }, [router.query])
+
+    return (snap.init?<>
         <Head>
             <title> {
                 `تفسير نمونه - ${
-                    props.data.namoonaTopic.title
+                    snap.data.namoonaTopic.title
                 }`
             }</title>
             <meta property="og:title"
                 content={
                     `تفسير نمونه - ${
-                        props.data.namoonaTopic.title
+                        snap.data.namoonaTopic.title
                     }`
             }></meta>
             <meta name="description"
                 content={
-                    props.data.namoonaTopic.text
+                    snap.data.namoonaTopic.text
             }></meta>
             <meta name="og:description"
                 content={
-                    props.data.namoonaTopic.text
+                    snap.data.namoonaTopic.text
             }></meta>
         </Head>
         <div id={
@@ -58,46 +78,35 @@ const TafseereNamoona = (props : {
                     styles.d3
                 }>
                     <h1 className='urdu'> {
-                        props.data.namoonaTopic.title
+                        snap.data.namoonaTopic.title
                     }</h1>
                     <div id={
                         styles.d4
                     }>
                         <h4> {
-                            props.data.namoonaTopic.range[0] !== -1 && props.data.namoonaTopic.range[0] !== props.data.namoonaTopic.range[1] ? `${
-                                props.data.namoonaTopic.range[1] + 1
+                            snap.data.namoonaTopic.range[0] !== -1 && snap.data.namoonaTopic.range[0] !== snap.data.namoonaTopic.range[1] ? `${
+                                snap.data.namoonaTopic.range[1] + 1
                             }-${
-                                props.data.namoonaTopic.range[0] + 1
-                            }` : (props.data.namoonaTopic.range[0] === -1 ? `Surah ${
-                                props.s + 1
-                            }` : (props.data.namoonaTopic.range[0] === props.data.namoonaTopic.range[1] ? props.data.namoonaTopic.range[1] + 1 : props.data.namoonaTopic.range.map((n) => n + 1).join('-')))
+                                snap.data.namoonaTopic.range[0] + 1
+                            }` : (snap.data.namoonaTopic.range[0] === -1 ? `Surah ${
+                                snap.s + 1
+                            }` : (snap.data.namoonaTopic.range[0] === snap.data.namoonaTopic.range[1] ? snap.data.namoonaTopic.range[1] + 1 : snap.data.namoonaTopic.range.map((n) => n + 1).join('-')))
                         } </h4>
                     </div>
                 </div>
                 <div id={styles.d5}>
-                    {props.data.namoonaTopic.text.split('\n').map((t, i)=><p key={i} className='urdu'>{t}</p>)}
+                    {snap.data.namoonaTopic.text.split('\n').map((t, i)=><p key={i} className='urdu'>{t}</p>)}
                 </div>
                 
             </div>
         </div>
-    </>);
-};
-
-export const getServerSideProps: GetServerSideProps = async function ({query}) {
-    const Query = returnQuery(Number(query ?. s), (query ?. id as string));
-
-    const { data } = await client.query({
-        query: Query,
-      });
-
-    return {
-        props: {
-            s: Number(query ?. s),
-            v: Number(query ?. v),
-            link: (query ?. id as string),
-            data
-        }
-    };
+    </>:<div style={
+            {
+                position: 'fixed',
+                top: '50vh',
+                right: '50vw'
+            }
+        }><Loader/></div>);
 };
 
 export default TafseereNamoona
