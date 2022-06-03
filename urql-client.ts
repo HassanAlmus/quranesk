89a14a086cb1b3b6cb0d0ac83e3060e530394ce2
Hassan Al Mosapeeh <hassanalmosapeeh@gmail.com>
2022-06-03 19:39:28 +0300

    l

--- urql-client.ts
+++ urql-client.ts
@@ -9,7 +9,7 @@ import {
   const isServerSide = typeof window === "undefined";
   const ssrCache = ssrExchange({ isClient: !isServerSide });
   const client = createClient({
-    url: "https://quranesk-api.herokuapp.com/",
+    url: "https://quranesk-api-3.herokuapp.com/",
     exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
     fetchOptions: () => {
       return { headers: {} };
